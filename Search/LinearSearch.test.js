a4eea5551a479f01eb6731db995dddf00b71be7d
Al Camara <Alhousseinec@gmail.com>
2023-01-02 13:08:32 -0600

    added to extra test cases

--- Search/LinearSearch.test.js
+++ Search/LinearSearch.test.js
@@ -6,3 +6,15 @@ test("Search Should return true with search for a \"Dog\" ", () =>{
     expect(animalSearch(animals,"Dog")).toBe(true);
 
 });
+
+test("Search Should return true with search for a \"Cat\" ", () =>{
+
+    expect(animalSearch(animals,"Cat")).toBe(true);
+
+});
+
+test("Search Should return false with search for a \"Tiger\" ", () =>{
+
+    expect(animalSearch(animals,"Tiger")).toBe(false);
+
+});
